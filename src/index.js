f9fa35d56e6a519bf59fd136ec49d7a472bb06e2
paularml <paula.rml@hotmail.com>
2023-10-30 23:40:58 -0300

    feat: adicionados arquivos base do servidor e dotenv

---
+++ src/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(process.env.PORT);
