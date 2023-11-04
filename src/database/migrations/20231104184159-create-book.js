155c7768971f6f67578acbe533402a8203694c1a
paularml <paula.rml@hotmail.com>
2023-11-04 16:16:52 -0300

    feat: estrutura da tabela de livros

---
+++ src/database/migrations/20231104184159-create-book.js
@@ -0,0 +1,42 @@
+'use strict';
+const User = require('../models/user')
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      author: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      gender: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Books');
+  }
+};
--- src/database/models/book.js
+++ src/database/models/book.js
@@ -1,29 +1,28 @@
 'use strict';
-const Book = require('./book');
+
 const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class Book extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Book.belongsTo(models.User, { foreignKey: 'userId' });// define association here
     }
   }
-  User.init({
-    fullName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    city: DataTypes.STRING,
-    state: DataTypes.STRING
+  Book.init({
+    title: DataTypes.STRING,
+    author: DataTypes.STRING,
+    gender: DataTypes.STRING,
+    userId: DataTypes.INTEGER
   }, {
     sequelize,
-    modelName: 'User',
-    tableName: "users"
+    modelName: 'Book',
+    tableName: 'books'
   });
-  return User;
+  return Book;
 };
