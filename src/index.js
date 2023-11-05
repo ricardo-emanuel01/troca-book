9755759c0c07f7af5fa46eecfda2e23c1b6e822b
Miguel-D3v <xmiguelxpeixotox@gmail.com>
2023-11-05 15:52:59 -0300

    adding helmet for api security

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const routes = require('./routes');
 const cors = require('cors');
+const helmet = require('helmet');
 
 const app = express();
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
