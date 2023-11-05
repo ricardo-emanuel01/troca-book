ef88beac1f21a55e106267efab022119bdb937bc
Miguel-D3v <xmiguelxpeixotox@gmail.com>
2023-11-05 13:13:50 -0300

    feat:adding CORS to avoid CROSS-ORIGIN error

--- src/server.js
+++ src/server.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const routes = require('./routes');
 const helmet = require('helmet');
-
+const cors = require('cors');
 const app = express();
 
 app.use(helmet())
+app.use(cors())
 app.use(express.json());
 
 app.use(routes);
