41253c24a1b1559febe531c0f7f2cd9a4c7d4ac6
Miguel-D3v <xmiguelxpeixotox@gmail.com>
2023-11-05 13:10:50 -0300

    feat:adding more security

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const routes = require('./routes');
+const helmet = require('helmet');
 
 const app = express();
 
+app.use(helmet())
 app.use(express.json());
 
 app.use(routes);
